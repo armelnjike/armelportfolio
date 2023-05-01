dd5c64897e0210233587c241c5039215b21891b1
ArmelNjike <armelnjike@localhost.localdomain>
2023-05-01 18:34:53 +0100

    sans contenue

--- src/containers/Certtifications/Certifications.jsx
+++ src/containers/Certtifications/Certifications.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
 import './Certifications.scss'
 
+import React, { useState, useEffect } from 'react'
+import { animate, motion } from 'framer-motion'
+import { Tooltip as ReactTooltip} from 'react-tooltip';
+
+import { MotionWrap } from '../../wrapper';
+import { AppWrap } from '../../wrapper'
+import { urlFor, client } from '../../client'
+import { images } from '../../constants';
+
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import HotelIcon from '@mui/icons-material/Hotel';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import Typography from '@mui/material/Typography';
+
+import { MDBContainer, MDBIcon } from "mdb-react-ui-kit";
+
+import 'react-vertical-timeline-component/style.min.css';
+
 const Certifications = () => {
+const [Certif, setCertif] = useState([])
+useEffect(() => {
+  const query = '*[_type == "certification"]';
+
+  return () => {
+    client.fetch(query).then((data) => {
+      setCertif(data)
+    })}})  
+
+
   return (
-    <div>Certifications</div>
+    <>
+    <h2 className="head-text">My <span> Certifications</span></h2>
+      <MDBContainer className="py-5">
+      <ul className="timeline-with-icons">
+      {  Certif?.map((item) => (
+        <li className="timeline-item mb-5">
+          <span className="timeline-icon">
+            <img src={ urlFor(item.institutionImg) } alt='intitution'  style={{ width: '70px'}}/>
+          </span>
+
+          <h5 className="fw-bold"> {item.certification} </h5>
+          
+          <p className="text-muted">
+            {item.description}
+          </p>
+          
+          <div style={{width:'50%',marginTop:'15px'}}>
+          <hr />
+          <p >
+            {item.state}
+          </p>
+          </div>
+          
+        </li>
+      )) }
+        
+
+      </ul>
+    </MDBContainer>
+    </>
   )
 }
 
--- src/containers/Education/Education.jsx
+++ src/containers/Education/Education.jsx
@@ -8,96 +8,108 @@ import { AppWrap } from '../../wrapper'
 import { urlFor, client } from '../../client'
 import { images } from '../../constants';
 
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import HotelIcon from '@mui/icons-material/Hotel';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import Typography from '@mui/material/Typography';
+
+
 const Skills = () => {
 
- /* const [experiences, setExperiences] = useState([]);
-  const [skills, setSkills] = useState([]);
+  const [education, setEducation] = useState([]);
+  const [education2, setEducation2] = useState([]);
 
   useEffect(() => {
     const query = '*[_type == "education"]';
-    const skillsQuery = '*[_type == "skills"]';
+    //const skillsQuery = '*[_type == "skills"]';
 
     client.fetch(query).then((data) => {
-      setExperiences(data);
-    });
+      setEducation(data);
+      setEducation2([...education].sort((a,b) => a.period - b.period))
+    })});
+    
 
-    client.fetch(skillsQuery).then((data) => {
-      setSkills(data);
-    });
-  }, []);*/
 
   return (
     <>
-      <h2 className="head-text">About my<span> Education</span></h2>
-
-      <div className='timeline'>
-
-        <div className='container left-container'>
-          <div className='text-box'>
-            <h2>Alphabet Inc</h2>
-            <small>2018 - 2019</small>
-            <p>rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrr</p>
-          </div>
-        </div>
-
-
-        <div className='container right-container'>
-          <div className='text-box'>
-            <h2>Alphabet Inc</h2>
-            <small>2018 - 2019</small>
-            <p>rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrr</p>
-          </div>
-        </div>
-
-
-
-        <div className='container left-container'>
-          <div className='text-box'>
-            <h2>Alphabet Inc</h2>
-            <small>2018 - 2019</small>
-            <p>rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrr</p>
-          </div>
-        </div>
-
-
-
-        <div className='container right-container'>
-          <div className='text-box'>
-            <h2>Alphabet Inc</h2>
-            <small>2018 - 2019</small>
-            <p>rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrr</p>
-          </div>
-        </div>
-
-
-
-        <div className='container left-container'>
-          <div className='text-box'>
-            <h2>Alphabet Inc</h2>
-            <small>2018 - 2019</small>
-            <p>rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
-            rrrrrrrrrrrrrrrrrrrrrrrrr</p>
-          </div>
-        </div>
-
-
-
+    <h2 className='head-text'>
+        About my 
+        <span> Education</span><br/> 
+      </h2>
+     <Timeline position="alternate">
+     { education2?.map((item,index) => (
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          align="right"
+          variant="body2"
+          color="text.secondary"
+        >
+          { item.period }
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot>
+            <FastfoodIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+          <div className="app__skills-exp">
+          
+          <motion.div
+            className="app__skills-exp-item"
+            
+          >
+          
+            <motion.div className="app__skills-exp-works">
+              
+                <>
+                  <motion.div
+                    whileInView={{ opacity: [0, 1] }}
+                    transition={{ duration: 0.5 }}
+                    className="app__skills-exp-work"
+                    data-tip
+                    
+                  >
+                  
+                    <h4 className="bold-text vertical-timeline-element-title"> { item.achievements } </h4>
+                    <p className="p-text vertical-timeline-element-subtitle"> { item.school } </p>
+                  </motion.div>
+                  <ReactTooltip
+                  
+                    effect="solid"
+                    arrowColor="#fff"
+                    className="skills-tooltip"
+                    
+                  >
+                    Dans la ville de Bangangté
+                  </ReactTooltip>
+                </>
+              
+            </motion.div>
+          </motion.div>
+        
       </div>
+          </Typography>
+        
+        </TimelineContent>
+      </TimelineItem>
+     )) }
       
+     </Timeline>
     </>
   );
 }
